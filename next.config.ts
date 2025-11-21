89629f361e811be0cd0e691248549ab46384d76d
MatiasLopez2 <matiaslopez0022@gmail.com>
2025-11-21 14:18:28 -0300

    next-config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+  return [
+    {
+      source: '/((?!customers/).*)', // todo menos /customers/
+      destination: '/dashboard',
+      permanent: false,
+    },
+  ];
+}
 };
 
 export default nextConfig;
