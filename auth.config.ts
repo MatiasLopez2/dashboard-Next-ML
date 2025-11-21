8266ad6061544d55514b4169afdf955f7c7d5794
MatiasLopez2 <matiaslopez0022@gmail.com>
2025-11-21 12:49:59 -0300

    login

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,5 @@
-// app/lib/auth.config.ts (o donde lo tengas)
 import type { NextAuthConfig } from 'next-auth';
-
+ 
 export const authConfig = {
   pages: {
     signIn: '/login',
@@ -9,31 +8,14 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-
       if (isOnDashboard) {
-        return isLoggedIn; // true si está logueado, false si no → redirige a /login
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        // Si está logueado y va a /login o /, lo redirige al dashboard
-        const isLoginPage = nextUrl.pathname === '/login';
-        if (isLoginPage || nextUrl.pathname === '/') {
-          return Response.redirect(new URL('/dashboard', nextUrl));
-        }
+        return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
     },
-    jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-        token.role = user; // opcional, si guardas rol
-      }
-      return token;
-    },
-    session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.id as string;
-      }
-      return session;
-    },
   },
-  providers: []
+  providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
